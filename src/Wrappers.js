9c8c05118e097084b4ecac57495478574ce122d4
coalcanvas <ansb1987@gmail.com>
2020-11-06 14:02:13 +0800

    Create Wrappers.js

---
+++ src/Wrappers.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const DonutWrapper = styled.svg`
+  background: none;
+  width: 600px;
+  height: 600px;
+  position: relative;
+`;
+
+export const parallaxWrapper = styled.div`
+  background: none;
+  width: 600px;
+  height: 600px;
+  padding-left: 300px;
+  position: relative;
+`;
