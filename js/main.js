820374e17d93c831f1e9e657754660afffb5f5f5
coalcanvas <ansb1987@gmail.com>
2020-11-16 04:33:52 +0800

    test

--- js/main.js
+++ js/main.js
@@ -1,96 +1,145 @@
-// gsap.from(".logo", { duration: 2, opacity: 0, scale: 0.3, backgroundColor: "#fff", borderRadius: "10%", border: "2px solid none", ease: "back"}); 
+console.clear();
 
-// // gsap.to(".logo", { duration: 2, y: -600, backgroundColor: "#fff", borderRadius: "10%", border: "2px solid none", easeIn: "back"});
+// Import all the neccessary modules
+import gsap from "gsap";
+import { loadImages, splitArray } from "./utils";
+import Canvas from "./canvas";
 
+// import the styles
+import "../styles.scss";
 
-// gsap.set(".logo", {scale:0.7});
+// ALL THE CARD OPTIONS LISTED BELOW -->
 
-// gsap.timeline().from(".logo", {opacity:0})
-//     .from("#title", {opacity:0, scale:0, ease:"back"})
+// 1. jumpToRandomPosition: If `true` the particles on every frame will jump to random position. Else the particles will move randomly without jumping. Defaults to `false`
+// 2. growAndShrink: If `true` the particles will grow & shrink, it will grow .8 times larger than the radius. Defaults to `false`
+// 3. fill: If `true` the particles are filled with the current pixel color that they are on, else they will be stroked for that same color. Defaults to `true`
+// 4. bounceFromEdges: If `true` the particles will bounce back when they hit the (specified) edges, or else thay will continue their path from the opposite edges/walls. Defaults to `true`.
+// 5. shape: You can specify what shape of the particles. Currently you can specify any one from the following, i.e. "circle", "square", "hexagon". If not specified then it defaults to "circle"
+// 6. radius: You can specity the radius of the particles, defaults to "5" if not specified.
+// 7. randomRadius: If `true` then the particles will have random radius, else defaults to `false`
+// 8. maxRadius: You can specify the minimum radius of the particles, else defaults to "2"
+// 9. minRadius: You can specify the maximum radius of the particles, else defaults to "5"
+// 10. maxVelocity: You can specify the maximum velocity of the particles, else defaults to "8"
 
-const overflow = document.querySelector("#overflow");
-const viewport = document.querySelector(".viewport");
-const wrapper  = document.querySelector(".wrapper");
-const boxes    = document.querySelector(".boxes");
-const proxy    = document.createElement("div");
+// Okay that's it, that's all the options I could add. Play around and see what fits for you.
+// Also please STAR this project if you think it's interesting, you can even fork it and make/add something new.
 
-const numBoxes  = 10;  
-const boxWidth  = 350;
-const boxHeight = 250;  
-const imgWidth  = boxWidth  - 6;
-const imgHeight = boxHeight - 14;
-const viewWidth = innerWidth;
-const wrapWidth = numBoxes * boxWidth;
-const wrapVal = gsap.utils.wrap(0, wrapWidth);
+const cards = Array.from(document.querySelectorAll(".card"));
 
-gsap.set([wrapper, viewport], { height: boxHeight, xPercent: -50 });
-gsap.set(boxes, { left: -boxWidth });
+const cardOptions = [
+	{
+		imageURL: {
+			default: require("../img/desert-dark.jpg"),
+			hovered: require("../img/desert-bright.jpg"),
+			// default: "https://source.unsplash.com/8xznAGy4HcY/400x600",
+			// hovered: "https://source.unsplash.com/Xc6gtOwSMSA/400x600",
+		},
+		totalParticles: 1500,
+		mouseRange: 80,
+		particlesConfig: {
+			jumpToRandomPosition: false,
+			fill: true,
+			randomRadius: true,
+			minRadius: 1,
+			maxRadius: 2,
+		},
+	},
+	{
+		imageURL: {
+			default: require("../img/forest.jpg"),
+			hovered: require("../img/forest-top.jpg"),
+			// default: "https://source.unsplash.com/wQImoykAwGs/400x600",
+			// hovered: "https://source.unsplash.com/QsWG0kjPQRY/400x600",
+		},
+		totalParticles: 1500,
+		particlesConfig: {
+			jumpToRandomPosition: true,
+			fill: true,
+			shape: "square",
+			radius: 2,
+		},
+	},
+	{
+		imageURL: {
+			default: require("../img/ocean-top.jpg"),
+			hovered: require("../img/ocean.jpg"),
+			// default: "https://source.unsplash.com/sLAk1guBG90/400x600",
+			// hovered: "https://source.unsplash.com/xe-ss5Tg2mo/400x600",
+		},
+		totalParticles: 2500,
+		particlesConfig: {
+			jumpToRandomPosition: false,
+			bounceFromEdges: false,
+			fill: false,
+			shape: "hexagon",
+			radius: 1,
+		},
+	},
+];
 
-for (let i = 1; i <= numBoxes; i++) {
-  const src = "https://unsplash.it/" + imgWidth + "/" + imgHeight + "?random=" + i;
-  const num = document.createElement("div");
-  num.className = "num";
-  num.innerText = i;
-  
-  const img = document.createElement("img");
-  img.src = src;
-  img.width = imgWidth;
-  img.height = imgHeight;
-  
-  const box = document.createElement("div");
-  box.className = "box";
-  
-  box.appendChild(img);
-  box.appendChild(num);
-  
-  boxes.appendChild(box);
+const imageURLS = cardOptions.map((option) => Object.values(option.imageURL)).flat();
+// after all img are loaded remove loader
+// (this is not the best way to do so but it gets the job done)
+loadImages(imageURLS, (img) => {
+	// this array holds the img in a sub array
+	// i.e [img, img, img, img, img, img] ==> [[img, img], [img, img], [img, img]]
+	const splitedImagesArray = splitArray(img, 2);
 
-  gsap.set(box, { x: i * boxWidth, width: boxWidth, height: boxHeight });
-}
+	cards.forEach((card, index) => {
+		new Canvas({
+			parent: card.querySelector(".card__image--inner"),
+			dimensions: {
+				width: card.getBoundingClientRect().width,
+				height: card.getBoundingClientRect().height,
+			},
+			...cardOptions[index],
+			img: {
+				default: splitedImagesArray[index][0],
+				hovered: splitedImagesArray[index][1],
+			},
+		});
+	});
 
-const animation = gsap.to(".box", {
-  duration: 1,
-  x: `+=${wrapWidth}`, 
-  ease: "none",
-  paused: true,
-  modifiers: {
-    x: function(x, target) {
-      x = parseInt(x) % wrapWidth;
-      target.style.visibility = x - boxWidth > viewWidth ? "hidden" : "visible";
-      return `${x}px`;
-    }
-  }
+	// hide the loading wrapper
+	document.querySelector(".loading__wrapper").classList.add("hide");
+
+	// let the gsap animation begin
+	gsap.timeline({
+		delay: 0.8,
+		defaults: {
+			duration: 1.5,
+			stagger: 0.1,
+			ease: "expo.out",
+		},
+	})
+		.fromTo(
+			cards.map((card) => card.querySelector(".card__image")),
+			{
+				translateY: "-100%",
+			},
+			{
+				translateY: "0%",
+			}
+		)
+		.fromTo(
+			cards.map((card) => card.querySelector(".card__image--inner")),
+			{
+				translateY: "100%",
+			},
+			{
+				translateY: "0%",
+			},
+			0
+		)
+		.fromTo(
+			cards.map((card) => card.querySelector(".card__text--inner")),
+			{
+				translateY: "100%",
+			},
+			{
+				duration: 1.2,
+				translateY: "0%",
+			},
+			0.4
+		);
 });
-
-Draggable.create(proxy, {
-  type: "x",
-  trigger: ".wrapper",
-  inertia: true,
-  onDrag: updateProgress,
-  onThrowUpdate: updateProgress,
-  snap: { 
-    x: (x) => {
-      return Math.round(x / boxWidth) * boxWidth;
-    } 
-  }
-});
-
-overflow.addEventListener("change", applyOverflow);
-window.addEventListener("resize", resize);
-
-function updateProgress() {
-  animation.progress(wrapVal(this.x) / wrapWidth);
-}
-
-function resize() {
-  viewWidth = viewport.offsetWidth;
-  animation.render(animation.time(), false, true);
-}
-
-function applyOverflow() {
-  if(overflow.checked) {
-    gsap.set(".wrapper", {overflow: "visible"});
-  } else {
-    gsap.set(".wrapper", {overflow: "hidden"});
-  }
-}
