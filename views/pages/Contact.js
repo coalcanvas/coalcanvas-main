ba1abaa5d5f3f3a8c52e8c2e60d1a51a99b2cdf5
amygdala <ansb1987@gmail.com>
2021-05-15 10:53:09 +0800

    form connections

--- views/pages/Contact.js
+++ views/pages/Contact.js
@@ -2,6 +2,7 @@
  * Fetch data from external API.
  * @return {Array} Data fetched.
  */
+
 const getItems = async () => {
   try {
     // Set API url.
@@ -57,12 +58,12 @@ const Contact = {
     </div>
      <div class="form-group">
         <label for="password" class='small-text'>Password</label>
-        <input type="current-password" class="form-control shorter xsmall-text text-muted" id="current-password" name="current-password" 
+        <input type="password" class="form-control shorter xsmall-text text-muted" id="current-password" name="currentPassword" 
           placeholder="insert your password to auto-create account">
     </div>
         <div class="form-group">
         <label for="password" class='small-text'>Confirm password</label>
-        <input type="text" class="form-control shorter xsmall-text text-muted" id="password" name="password"
+        <input type="password" class="form-control shorter xsmall-text text-muted" id="password" name="password"
           placeholder="confirm your password">
     </div>
 
@@ -75,13 +76,13 @@ const Contact = {
             <small class="form-text text-muted xsmall-text">
            *required fields. <br/>
            our e-mail service provider: <br/>
-                   <img class='p-1' src='https://sendgrid.com/brand/sg-twilio/SG_Twilio_Lockup_RGBx1.png' width='auto' height='25px'/>
+                   <img class='p-1' src='https://image.flaticon.com/icons/png/512/281/281769.png' width='auto' height='25px'/>
         </small>
 
 
         <div class="form-group form-check">
         <input type="checkbox" class="form-check-input" 
-               id="newsletter" name="newsletter" checked value='checked'>
+               id="newsletter" name="newsletter" checked >
         <label class="form-check-label xsmall-text" for="newsletter">
 signup for newsletters/updates?</label> </div>
 
@@ -130,14 +131,17 @@ signup for newsletters/updates?</label> </div>
     const btn = document.querySelector("#btn");
     const username = document.querySelector("#username");
     const email = document.querySelector("#email");
-    const address = document.querySelector("#address");
+    const address = document.querySelector("address");
     const message = document.querySelector("#message");
     const newsletter = document.querySelector("#newsletter");
     const password = document.querySelector("#current-password");
     const confirm_password = document.querySelector("#password");
-    const url = config[0].APPSCRIPT_WEB_URL;
+    const prefix = "https://script.google.com/macros/s/";
+    const suffix = "/exec";
+    const url = prefix + config[0].APPSCRIPT_DEPLOYMENT_ID + suffix;
+    const headers = config[0].headers;
     console.log(url);
-    // GET request
+    // console.log(newsletter.value);
 
     // handlesubmit, mutation
     myForm.addEventListener("submit", (e) => {
@@ -145,8 +149,59 @@ signup for newsletters/updates?</label> </div>
       const ele = myForm.elements;
       // console.log(ele);
       // console.log("sending data...");
+      //set initial state of objects
       const holder = {};
       const err = [];
+      const suc = [];
+
+      //set error notification functionality
+      function notice() {
+        err.forEach((error) => {
+          //add error message into output div
+          output.style.display = "block";
+          output.innerHTML += error + "<br/>";
+          output.style.borderRadius = "12px";
+          output.style.transform = "translateY(0px)";
+          output.style.transition =
+            "all .4s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s 1";
+          output.style.backgroundColor = "salmon";
+        });
+      }
+      function noticeOut() {
+        err.forEach(() => {
+          output.style.display = "none";
+          output.innerHTML = "";
+          output.style.transform = "translateY(-20px)";
+          output.style.transition =
+            "all .4s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s 1";
+          output.style.backgroundColor = "transparent";
+        });
+      }
+
+      //set submit success notification
+      function success() {
+        suc.forEach((success) => {
+          //add error message into output div
+          output.style.display = "block";
+          output.innerHTML += success + "<br/>";
+          output.style.borderRadius = "12px";
+          output.style.transform = "translateY(0px)";
+          output.style.transition =
+            "all .4s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s 1";
+          output.style.backgroundColor = "chartreuse";
+        });
+      }
+      function successOut() {
+        suc.forEach(() => {
+          output.style.display = "none";
+          output.innerHTML = "";
+          output.style.transform = "translateY(-20px)";
+          output.style.transition =
+            "all .4s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s 1";
+          output.style.backgroundColor = "transparent";
+        });
+      }
+
       // interate through el in form
       for (let i = 0; i < ele.length; i++) {
         // console.log(ele[i]);
@@ -165,11 +220,13 @@ signup for newsletters/updates?</label> </div>
             err.push("username must be at least 6 characters");
           }
         }
-        if (el.name == "newsletter") {
-          if (!el.checked) {
+        if (el.name == "") {
+          val = false;
+        }
+        if (el.name == "password") {
+          if (el.value != password.value) {
             val = false;
-          } else {
-            val = true;
+            err.push("passwords must match.");
           }
         }
         //check conditions
@@ -187,40 +244,65 @@ signup for newsletters/updates?</label> </div>
       }
       if (err.length > 0) {
         output.innerHTML = "";
-        err.forEach((error) => {
-          //add error message into output div
-          output.innerHTML += error + "<br/>";
-          output.style.borderRadius = "12px";
-          output.style.transform = "translateY(0px)";
-          output.style.transition =
-            "all 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275)";
-          output.style.backgroundColor = "salmon";
-        });
+        notice();
+        setTimeout(function () {
+          noticeOut();
+        }, 1500);
       } else {
         // form submit
         console.log(holder);
-        fetch(url, {
-          method: "POST",
-          body: JSON.stringify(holder),
-        })
+        suc.push(
+          "Form successfully submitted. We will get back to you soon via email."
+        );
+        fetch(
+          url,
+          // headers,
+          // { mode: "no-cors" },
+          {
+            method: "POST",
+            body: JSON.stringify(holder),
+          }
+        )
           .then((res) => res.json())
           .then((data) => {
             console.log(data);
+            success();
+            setTimeout(function () {
+              successOut();
+              clearForm();
+            }, 1500);
           });
       }
     });
 
-    newsletter.addEventListener("click", function toggleNl(e) {
-      e.preventDefault();
-      const newsletter = e.target;
-      if (newsletter.hasAttribute("checked") === true) {
-        newsletter.setAttribute("checked", false);
-        newsletter.value = "unchecked";
-      } else {
-        newsletter.setAttribute("checked", true);
-        newsletter.value = "checked";
+    //execute notification
+
+    //clear form
+    function clearForm() {
+      const ele = myForm.elements;
+      output.style.display = "none";
+      output.innerHTML = "";
+      for (let i = 0; i < ele.length; i++) {
+        if (ele[i].getAttribute("type") != "submit") {
+          ele[i].value = "";
+        }
       }
-    });
+    }
+    //toggle newsletter value
+    function toggleNl(e) {
+      const newsletter = e.target;
+
+      if (newsletter.checked === true) {
+        newsletter.checked === false;
+        newsletter.value = "on";
+      } else if (newsletter.checked === false) {
+        newsletter.checked === true;
+        newsletter.value = "off";
+      }
+      // console.log("cick", e.target.value);
+    }
+
+    newsletter.addEventListener("click", toggleNl);
     function validateEmail(email) {
       //regular expression
       const re = /\S+@\S+\.\S+/;
