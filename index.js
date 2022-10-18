c450f8afcf21d7dd97fec5e65f7d9df0eabdb1f8
amygdala <ansb1987@gmail.com>
2022-10-19 07:19:24 +0800

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import gsap from "gsap";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import * as dat from "dat.gui";
 import "./style.css";
 import "./swiper.css";
