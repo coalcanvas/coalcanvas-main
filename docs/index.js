6db0a30a6a4884ea38e5c0f3f2df0867f5165636
coalcanvas <ansb1987@gmail.com>
2022-10-19 11:11:53 +0800

    Add files via upload

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import dat from "dat.gui";
+import * as dat from "dat.gui";
 import style from "/style.css" assert { type: "css" };
 import swiperModule from "/swiper.css" assert { type: "css" };
 
