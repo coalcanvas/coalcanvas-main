f981250655a9d92620746c89cabecf656b86ee2e
coalcanvas <ansb1987@gmail.com>
2022-10-19 14:44:29 +0800

    Add files via upload

--- docs/index.js
+++ docs/index.js
@@ -2,7 +2,6 @@ import * as THREE from "three";
 import gsap from "gsap";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 // import * as dat from "dat.gui";
-import Swiper from "swiper";
 
 let className = [
   "icon ion-ios-home-outline",
